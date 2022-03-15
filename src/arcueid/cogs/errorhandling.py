4c1590d2e21ac0a687fbeb51809fcf8f45f46630
Hazel Rella <hazelrella11@gmail.com>
2022-03-15 13:05:27 -0700

    Added basic error handling

---
+++ src/arcueid/cogs/errorhandling.py
@@ -0,0 +1,45 @@
+import traceback
+from typing import Optional, cast, Type
+
+import discord
+import discord.ext.commands as comms
+
+from .abc import ACog
+
+from ..context import ArcContext
+
+__all__ = [
+    'ErrorHandlingCog'
+]
+
+
+class ErrorHandlingCog(ACog):
+    @comms.Cog.listener()
+    async def on_command_error(self, ctx: ArcContext, error: comms.CommandError) -> None:
+        if ctx.errorHandled:
+            return
+
+        match error:
+            case comms.CommandNotFound():
+                return  # Avoid spamming on every Arcueid Ping
+            case comms.CommandOnCooldown():
+                title = 'Command on Cooldown'
+                description = 'This command is on cooldown please try again in ' \
+                              f'{round(cast(comms.CommandOnCooldown, error).retry_after, 1)} seconds.'
+            case comms.MissingPermissions():
+                title = 'Missing Permissions'
+                description = 'You are missing required permissions to run the given command.'
+            case comms.UserInputError():
+                title = 'User Input Error'
+                description = 'Something about your input was wrong, please check and try again.'
+            case _:
+                title = f'Command Error ({type(error).__name__})'
+                description = 'Something went wrong with the command you entered. '\
+                              f'```{"".join(traceback.format_exception(cast(Type[BaseException], error), limit=1))}```' \
+                              'Please contact Harlot#0001 about this.'
+
+        await ctx.replyEmbed(title=title, description=description, error=True)
+
+    @property
+    def color(self) -> Optional[discord.Color]:
+        return discord.Color(0xff1717)
--- src/arcueid/context.py
+++ src/arcueid/context.py
@@ -9,16 +9,23 @@ __all__ = [
 
 class ArcContext(comms.Context):
     """Extended Arcueid Command Context."""
-    def generateEmbed(self, title: str, description: str) -> discord.Embed:
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.errorHandled = False
+
+    def generateEmbed(self, title: str, description: str, *, error: bool = False) -> discord.Embed:
         """Generate a thememed Discord embed"""
+        if error:
+            color = discord.Color(0xff1717)
+        else:
+            color = getattr(self.cog, 'color', discord.Color(self.bot.settings.theme))
 
-        color = getattr(self.cog, 'color', discord.Color(self.bot.settings.theme))
-
-        if color is None:
-            color = discord.Color(self.bot.settings.theme)
+            if color is None:
+                color = discord.Color(self.bot.settings.theme)
 
         return discord.Embed(title=title, description=description, color=color)
 
-    async def replyEmbed(self, title: str, description: str) -> None:
+    async def replyEmbed(self, title: str, description: str, *, error: bool = False) -> None:
         """Reply using a themed Discord embed"""
-        await self.reply(embed=self.generateEmbed(title, description))
+        await self.reply(embed=self.generateEmbed(title, description, error=error))
